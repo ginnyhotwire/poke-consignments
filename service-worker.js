1b77d824e8be229494244e2dca3babcbc2fd6de8
ginnyhotwire <ginny.hooi@hotwireglobal.com>
2026-03-13 23:13:57 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,21 @@ caches.open(CACHE).then(cache=>cache.addAll(files))
 )
 })
 
+/* Delete old caches function */
+self.addEventListener("activate",event=>{
+event.waitUntil(
+caches.keys().then(keys=>{
+return Promise.all(
+keys.map(key=>{
+if(key!==CACHE){
+return caches.delete(key)
+}
+})
+)
+})
+)
+})
+
 self.addEventListener("fetch",e=>{
 e.respondWith(
 caches.match(e.request).then(r=>r||fetch(e.request))
